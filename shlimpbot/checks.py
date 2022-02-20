e4ead1b5460e486433ca374cda1f84c1b37456ee
MoxAmber <amber@dahou.se>
2022-02-20 23:43:07 +0000

    checks: is_config_channel always returns true outside guilds

--- shlimpbot/checks.py
+++ shlimpbot/checks.py
@@ -5,6 +5,9 @@ from shlimpbot.bot import config
 
 def is_config_channel(config_key: str):
     async def check_channel(ctx: commands.Context):
+        if not ctx.guild:
+            # We're in a DM of some sort, so it's always allowed.
+            return True
         config_channel = config.get_server(ctx.guild.id, config_key)
         return ctx.channel.id == config_channel
 
